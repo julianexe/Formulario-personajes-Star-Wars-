1310572166165fd9c49d282c75411d20f7b7743c
Julián Morales <julianmoralesvega@gmail.com>
2021-10-06 18:29:23 -0500

    formulario y algo de apex
    aun no integro la api pq ni idea

---
+++ StarWars/force-app/main/default/lwc/formularioPersonajes/formularioPersonajes.js
@@ -0,0 +1,45 @@
+import { LightningElement, wire, api } from 'lwc';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import getPersonajes from '@salesforce/apex/StarController.getPersonajes'
+import NUMBER_FIELD from '@salesforce/schema/Contact.number_character__c';
+import NAME_FIELD from '@salesforce/schema/Contact.LastName';
+import HEIGHT_FIELD from '@salesforce/schema/Contact.height__c';
+import GENDER_FIELD from '@salesforce/schema/Contact.gender__c';
+import HAIR_FIELD from '@salesforce/schema/Contact.hair_color__c';
+import EYE_FIELD from '@salesforce/schema/Contact.eye_color__c';
+import URL_FIELD from '@salesforce/schema/Contact.url__c';
+import HOME_FIELD from '@salesforce/schema/Contact.homeworld__c';
+
+ 
+export default class lwcContact extends LightningElement {
+ 
+    contactColumns = [
+
+        { label: 'Number Character', fieldName: 'number_character__c' },
+        { label: 'Name', fieldName: 'LastName' },
+        { label: 'Height', fieldName: 'height__c' },
+        { label: 'Gender', fieldName: 'gender__c' },
+        { label: 'Hair Color', fieldName: 'hair_color__c' },
+        { label: 'Eye Color', fieldName: 'eye_color__c' },
+        { label: 'URL', fieldName: 'url__c' },
+        { label: 'Home World', fieldName: 'homeworld__c' },
+        
+    ];
+ 
+    fields = [NAME_FIELD, HEIGHT_FIELD, GENDER_FIELD, HAIR_FIELD, EYE_FIELD, URL_FIELD, HOME_FIELD, NUMBER_FIELD];
+ 
+    @wire(getPersonajes)
+    contacts;
+ 
+    @api objectApiName;
+ 
+    handleSuccess(event) {
+        const evt = new ShowToastEvent({
+            title: "Character Created",
+            message: "Record ID: " + event.detail.id,
+            variant: "success"
+        });
+        this.dispatchEvent(evt);
+    }
+ 
+}
